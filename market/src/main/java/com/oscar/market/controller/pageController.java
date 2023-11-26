eb0c96e626525eea89b869390f46af3704d114b0
LinGaOscar <oscar987123@yahoo.com.tw>
2023-11-26 23:15:47 +0800

    build index paodlist shopcart

--- market/src/main/java/com/oscar/market/controller/pageController.java
+++ market/src/main/java/com/oscar/market/controller/pageController.java
@@ -4,6 +4,8 @@ import com.oscar.market.model.NavItem;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,18 +13,58 @@ import java.util.List;
 
 @Controller
 public class pageController {
-    @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("headerTemplate", "headers/header");
-        model.addAttribute("contentTemplate", "contents/index");
-        model.addAttribute("footerTemplate", "footers/footer");
+    @GetMapping({"", "/", "/{page}"})
+    public String home(Model model, @PathVariable(required = false) String page) {
+        String pageName = pageFilter(page);
 
+        if (pageName.equals("redirect:")) {
+            return "redirect:/"; // 導向根目錄
+        }
+        buildPage(model, pageName);
+        return "main";
+    }
+
+    private void buildPage(Model model, String pageName) {
+        buildNav(model, pageName);
+        buildHeader(model, pageName);
+        buildContents(model, pageName);
+        buildFooter(model, pageName);
+    }
+
+    private String pageFilter(String page) {
+        // 判斷跟目錄
+        if (page == null || page.isEmpty()) {
+            return "index";
+        }
+        // 判斷現有頁面
+        if (page.equals("productList") || page.equals("shoppingCart")) {
+            return page;
+        } else {
+            return "redirect:";
+        }
+    }
+
+    private void buildNav(Model model, String pageName) {
+        if (pageName.equals("")) {
+
+        }
         // 動態生成連結的清單
         List<NavItem> navItems = new ArrayList<>();
         navItems.add(new NavItem("首頁", "/"));
-        navItems.add(new NavItem("商品1", "/product1"));
-        navItems.add(new NavItem("商品2", "/product2"));
+        navItems.add(new NavItem("商品清單", "/productList"));
+        navItems.add(new NavItem("購物車", "/shoppingCart"));
         model.addAttribute("navItems", navItems);
-        return "main";
+    }
+
+    private void buildFooter(Model model, String pageName) {
+        model.addAttribute("footerTemplate", "footers/footer");
+    }
+
+    private void buildContents(Model model, String pageName) {
+        model.addAttribute("contentTemplate", "contents/" + pageName);
+    }
+
+    private void buildHeader(Model model, String pageName) {
+        model.addAttribute("headerTemplate", "headers/header");
     }
 }
---
+++ market/src/main/java/com/oscar/market/controller/productListController.java
@@ -0,0 +1,7 @@
+package com.oscar.market.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class productListController {
+}
