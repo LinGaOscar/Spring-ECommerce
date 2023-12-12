6074ef34c3085001be5c565e6e2b15c3ad6ce7d3
LinGaOscar <oscar987123@yahoo.com.tw>
2023-12-12 09:01:37 +0800

    update database envi yml

--- database/src/main/java/com/oscar/database/exception/ControllerExceptionHandler.java
+++ database/src/main/java/com/oscar/database/exception/ControllerExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.oscar.database.exception;
 
 import com.oscar.database.model.ApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,21 +10,25 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ControllerExceptionHandler {
     ApiResponse response;
+    private Logger log = LoggerFactory.getLogger(this.getClass());
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ApiResponse> handleNotFoundException(NotFoundException e) {
+        log.info("查無資料,{}", e.getMessage());
         response = new ApiResponse("204", "查無資料", e.getMessage());
         return ResponseEntity.ok(response);
     }
 
     @ExceptionHandler(ParameterException.class)
-    public ResponseEntity<ApiResponse> handleParameterException(ParameterException e){
+    public ResponseEntity<ApiResponse> handleParameterException(ParameterException e) {
+        log.info("參數錯誤,{}", e.getMessage());
         response = new ApiResponse("422", "參數錯誤", e.getMessage());
         return ResponseEntity.ok(response);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse> handleException(Exception e) {
+        log.info("DB連線異常,{}", e.getMessage());
         response = new ApiResponse("500", "DB連線異常", e.getMessage());
         return ResponseEntity.ok(response);
     }
